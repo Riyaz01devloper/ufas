bceb2be9b02bad8e8c2f184119cf05584cf6ea5b
Md Sameem <mohammadsameem559@gmail.com>
2026-09-06 04:51:41 +0530

    added coa2

---
+++ frontend/src/pages/add-coa/addCoa.jsx
@@ -0,0 +1,118 @@
+import { useState } from "react";
+import { useAuth } from "../../context/AuthContext.jsx";
+import styles from "./addCoa.module.css";
+import { useNavigate } from "react-router";
+import addCoa from "../../api/addCoa.js";
+
+function AddCoa() {
+    const { accessToken, refreshAccessToken, user } = useAuth();
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        accountName: "",
+        accountType: ""
+    });
+    const [errors, setErrors] = useState({});
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+    const moveBack = () => {
+        setFormData({
+            accountName: "",
+            accountType: ""
+        });
+        navigate("/");
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const accountData = {
+            accountName: formData.accountName,
+            accountType: formData.accountType
+        };
+        console.log("Submitting account data:", accountData);
+        try {
+            let response = await addCoa(accountData, accessToken);
+            if (response.status === 401) {
+                const newAccessToken = await refreshAccessToken();
+                response = await addCoa(accountData, newAccessToken);
+            }
+            if (user.role !== "ADMIN" && user.role !== "ACCOUNTANT") {
+                throw new Error("You do not have permission to create a chart of accounts");
+                navigate("/");
+            }
+            const data = await response.json();
+            if (!response.ok) {
+                throw new Error(data.message || "Failed to create chart of accounts");
+            }
+        } catch (error) {
+            console.error("Error creating chart of accounts:", error.message);
+            setErrors(error.message);
+        }
+    };
+
+    return (
+        <div className={styles.addCoa}>
+            <h1>Create Chart of Accounts Form</h1>
+            {errors && <p className={styles.error}>{errors}</p>}
+            <div className={styles.formContainer}>
+                <form
+                    id="contact-form"
+                    className={styles.contactForm}
+                    onSubmit={handleSubmit}
+                >
+                    {/* Account Name */}
+                    <div className={styles.formField}>
+                        <label>Account Name</label>
+                        <input
+                            type="text"
+                            name="accountName"
+                            placeholder="Enter name"
+                            value={formData.accountName}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    {/* Account Type */}
+                    <div className={styles.formField}>
+                        <label>Account Type</label>
+                        <input
+                            type="text"
+                            name="accountType"
+                            placeholder="Enter account type"
+                            value={formData.accountType}
+                            onChange={handleChange}
+                        />
+                    </div>
+                </form>
+
+                <div className={styles.header}>
+                    <div className={styles.headerButtons}>
+                        <button
+                            type="button"
+                            className={`${styles.button} ${styles.newButton}`}
+                            onClick={moveBack}
+                        >
+                            Back
+                        </button>
+
+                        <button
+                            type="submit"
+                            form="contact-form"
+                            className={`${styles.button} ${styles.confirmButton}`}
+                        >
+                            Create Chart of Accounts
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AddCoa;
