58e1c7159f271f14e6b437e72301d22ece562e37
Riyaz <riyazmalik2046@gmail.com>
2026-09-05 15:55:59 +0530

    vendor form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+// import routes from "./routes.jsx";
+// import { RouterProvider } from "react-router-dom";
 
 function App() {
   return <div>hello</div>;
--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -2,7 +2,7 @@ import App from "./App";
 import ErrorPage from "./components/ErrorPage";
 import LoginPage from "./pages/login/login.jsx";
 import RegisterPage from "./pages/register/register.jsx";
-import VendorForm from "./pages/vendorForm/venderForm.jsx";
+import VendorForm from "./pages/vendorForm/vendorForm.jsx";
 
 const routes = [
   {
@@ -22,7 +22,7 @@ const routes = [
     element: <ErrorPage />,
   },
   {
-    path: "/vendor-form",
+    path: "/vendorform",
     element: <VendorForm />,
   },
 ];
